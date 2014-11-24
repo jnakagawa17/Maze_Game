e3f9972b374ab058ee00dad5f31a9f1c0797eddd
jnakagawa17 <jnakagawa17@punahou.edu>
2014-11-24 10:10:04 -1000

    Variables
    I renamed a lot of the variables so you can tell what they do.

--- Maze_Game/Maze_Game.ino
+++ Maze_Game/Maze_Game.ino
@@ -1,19 +1,24 @@
 #include <MeggyJrSimple.h>
-int x = 3;
-int y = 3;
-int z = 1;
-int a = 3;
-int b = 3;
-int c = 0;
+int playerX = 3;
+int playerY = 3;
+int playerColor = 1;
+int screenColor = 0;
+int savedX = 3;
+int savedY = 3;
+int savedColor = 0;
 int e = 0;
 int f = 0;
 int g = 0;
 int h = 0;
 int checkFree = 0;
-int heading = 0;
-int minionAvailable = 0;
-int minionX = 100;
-int minionY = 100;
+int directions = 0;
+int gameState = 1;
+int minionX = 0;
+int minionY = 0;
+int trail = 0;
+int minionLives = 3;
+int minionTurns = 8;
+boolean deadEnd = false;
 
 void setup()
 {
@@ -25,7 +30,20 @@ void setup()
 
 void loop()
 {
-  shift(); 
+  if(gameState == 1)
+  {
+    shift(); 
+  }
+  if(gameState == 2)
+  {
+    heading();
+  }
+  if(gameState == 3)
+  {
+    screenSaver();
+    DisplaySlate();
+    delay(500);
+  }
 }
 
 void drawMaze()
@@ -47,11 +65,11 @@ void drawMaze()
 
 void drawDot()
 {
-  DrawPx(a, b, c);
-  a = x;
-  b = y;
-  c = ReadPx(x, y);
-  DrawPx(x, y, z); 
+  DrawPx(savedX, savedY, savedColor);
+  savedX = playerX;
+  savedY = playerY;
+  savedColor = ReadPx(playerX, playerY);
+  DrawPx(playerX, playerY, playerColor); 
   DisplaySlate();
 }
 
@@ -60,24 +78,30 @@ void shift()
   CheckButtonsPress();
   if(Button_Left)
   {
-    if (x > 0)
-      x = x - 1;
+    CheckButtonsDown();
+    if(Button_Up)
+    {
+      gamestate = 2;
+      spawn();
+    }
+    if (playerX > 0)
+      playerX = playerX - 1;
       drawDot();
   }
   if(Button_Right)
   {
-    if (x < 7)
-      x = x + 1;
+    if (playerX < 7)
+      playerX = playerX + 1;
       drawDot();
   }
   if(Button_Up)
-    if (y < 7)
-      y = y + 1;
+    if (playerY < 7)
+      playerY = playerY + 1;
       drawDot();
   if(Button_Down)
   {
-    if (y > 0)
-      y = y - 1;
+    if (playerY > 0)
+      playerY = playerY - 1;
       drawDot();
   }
   if(Button_B)
@@ -85,192 +109,151 @@ void shift()
     CheckButtonsDown();
     if(Button_A)
     {
-      ScreenSaver();
+      gameState = 3;
     }
-    if (z < 4)
-      z = z + 1;
+    if (playerColor < 4)
+      playerColor = playerColor + 1;
       drawDot();
-    if (z == 4)
-      z = 0;
+    if (playerColor == 4)
+      playerColor = 0;
       drawDot();
   }
   if(Button_A)
   {
-    c = z;
+    savedColor = playerColor;
   }
 }
 
-void ScreenSaver()
+void screenSaver()
 {
     for (int a = 1; a < 7; a++)
     {
-      DrawPx(1, a, x);
-      DrawPx(a, 1, x);
-      DrawPx(a, 6, x);
-      DrawPx(6, a, x);
-      x = random(0, 7);
+      DrawPx(1, a, screenColor);
+      DrawPx(a, 1, screenColor);
+      DrawPx(a, 6, screenColor);
+      DrawPx(6, a, screenColor);
+      screenColor = random(0, 7);
     }
     for (int b = 0; b < 8; b++)
     {
-      DrawPx(0, b, x);
-      DrawPx(b, 0, x);
-      DrawPx(b, 7, x);
-      DrawPx(7, b, x);
-      x = random(0, 7);
+      DrawPx(0, b, screenColor);
+      DrawPx(b, 0, screenColor);
+      DrawPx(b, 7, screenColor);
+      DrawPx(7, b, screenColor);
+      screenColor = random(0, 7);
     }
     for (int c = 2; c < 6; c++)
     {
-      DrawPx(2, c, x);
-      DrawPx(c, 2, x);
-      DrawPx(c, 5, x);
-      DrawPx(5, c, x);
-      x = random(0, 7);
+      DrawPx(2, c, screenColor);
+      DrawPx(c, 2, screenColor);
+      DrawPx(c, 5, screenColor);
+      DrawPx(5, c, screenColor);
+      screenColor = random(0, 7);
     }
     for (int d = 0; d < 4; d++)
     {
-      DrawPx(3, 3, x);
-      DrawPx(4, 3, x);
-      DrawPx(3, 4, x);
-      DrawPx(4, 4, x);
-      x = random(0, 7);
+      DrawPx(3, 3, screenColor);
+      DrawPx(4, 3, screenColor);
+      DrawPx(3, 4, screenColor);
+      DrawPx(4, 4, screenColor);
+      screenColor = random(0, 7);
     }
     CheckButtonsDown();
-    if(Button_A)
+    if(Button_Up)
     {
-      shift();
+      ClearSlate();
+      playerX = 3;
+      playerY = 3;
+      playerColor = 1;
+      drawMaze();
+      DrawPx(3, 3, 1);
+      DisplaySlate();
+      gameState = 1;
     }
-    DisplaySlate();
-    delay(500);
-    ScreenSaver();
 }
 
 void spawn()
-{
-  DrawPx(0, 0, Blue);
-  minionAvailable++;
-}
-
-void identification()
 {
   for (int r = 0; r < 8; r++)
   {
     for (int s = 0; s < 8; s++)
     {
       if ( ReadPx(s, r) == Blue)
-        minionX = s;
-        minionY = r; 
+      {
+        heading();
+      }
+      else
+      {
+        DrawPx(0, 0, Blue);
+        minionX = 0;
+        minionY = 0;
+        DisplaySlate();
+      }
     }
   }
 }
 
-void pathing()
+void heading()
 {
-  checkFree = e + 1;
-  if ( ReadPx(checkFree, f) == 0)
-    right = true;
-    g++;
-  checkFree = e - 1;
-  if ( ReadPx(checkFree, f) == 0)
-    left = true;
-    g++;
-  checkFree = f + 1;
-  if ( ReadPx(e, checkFree) == 0)
-    up = true;
-    g++;
-  checkFree = f - 1;
-  if ( ReadPx(e, checkFree) == 0)
-    down = true;
-    g++;
-  heading = random(g);
+  if( ReadPx(minionX + 1, minionY) == 0)
+  {
+    h = h + 1;
+  }
+  if( ReadPx(minionX - 1, minionY) == 0)
+  {
+    h = h + 1;
+  }
+  if( ReadPx(minionX, minionY + 1) == 0)
+  {
+    h = h + 1;
+  }
+  if( ReadPx(minionX, minionY - 1) == 0)
+  {
+    h = h + 1;
+  }
+  if(h == 1)
+  {
+    deadEnd = true;
+  }
+  if(h > 1)
+  {
+    deadEnd = false;
+  }
+  directions = random(0, 4);
+  if (directions == 0)
+  {
+    minionY = minionY + 1;
+    if ( ReadPx(minionX, minionY) == 0)
+      moveMinion();
+  }
+  if (directions == 1)
+  {
+    minionY = minionY - 1;
+    if ( ReadPx(minionX, minionY) == 0)
+      moveMinion();
+  }
+  if (directions == 2)
+  {
+    minionY = minionX + 1;
+    if ( ReadPx(minionX, minionY) == 0)
+      moveMinion();
+  }
+  if (directions == 3)
+  {
+    minionY = minionX - 1;
+    if ( ReadPx(minionX, minionY) == 0)
+      moveMinion();
+  }
 }
 
 void moveMinion()
 {
-  if (heading = 0)
- {
-   if (right == true)
-   {
-     e = e + 1;
-   }
-   if (left == true)
-   {
-     e = e - 1;
-   }
-   if (up == true)
-   {
-     f = f + 1;
-   }
-   if (down == true)
-   {
-     f = f - 1;
-   }
- } 
-  if (heading = 1)
- {
-   if (left == true)
-   {
-     e = e + 1;
-   }
-   if (up == true)
-   {
-     e = e - 1;
-   }
-   if (down == true)
-   {
-     f = f + 1;
-   }
-   if (right == true)
-   {
-     f = f - 1;
-   }
- } 
-  if (heading = 2)
- {
-   if (up == true)
-   {
-     e = e + 1;
-   }
-   if (down == true)
-   {
-     e = e - 1;
-   }
-   if (right == true)
-   {
-     f = f + 1;
-   }
-   if (left == true)
-   {
-     f = f - 1;
-   }
- } 
-  if (heading = 3)
- {
-   if (down == true)
-   {
-     e = e + 1;
-   }
-   if (right == true)
-   {
-     e = e - 1;
-   }
-   if (left == true)
-   {
-     f = f + 1;
-   }
-   if (up == true)
-   {
-     f = f - 1;
-   }
- } 
- DrawPx(e, f, Blue);
- DisplaySlate();
- delay(300);
- if (h == minionAvailable)
- {
-   pathing();
- }
-}
-
-void exit()
-{
+  if(deadEnd == true)
+  {
+    DrawPx(minionX, minionY, Red);
+  }
+  if(deadEnd == false)
+  {
+    DrawPx(minionX, minionY, Blue);
+  }
 }
